c9f4ae654f517f79276ba95685b56acfae02e120
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-21 01:13:08 -0500

    fixed saving to localstorage and able to retriieve it

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   // create save button
   $(".save-btn").on("click", function () {
     console.log(this);
-    var currentHour = $(this).siblings(".hourCol").text();
+    var currentHour = $(this).parent().attr("id");
     console.log(currentHour);
     var currentText = $(this).siblings(".taskDescription").val();
     console.log(currentText);
@@ -15,8 +15,21 @@ $(document).ready(function () {
     localStorage.setItem(currentHour, currentText);
   });
 
-
   // load local storage data into each hour
+
+  $("#hour5 .taskDescription").val(localStorage.getItem("hour5"));
+  $("#hour6 .taskDescription").val(localStorage.getItem("hour6"));
+  $("#hour7 .taskDescription").val(localStorage.getItem("hour7"));
+  $("#hour8 .taskDescription").val(localStorage.getItem("hour8"));
+  $("#hour9 .taskDescription").val(localStorage.getItem("hour9"));
+  $("#hour10 .taskDescription").val(localStorage.getItem("hour10"));
+  $("#hour11 .taskDescription").val(localStorage.getItem("hour11"));
+  $("#hour12 .taskDescription").val(localStorage.getItem("hour12"));
+  $("#hour1 .taskDescription").val(localStorage.getItem("hour1"));
+  $("#hour2 .taskDescription").val(localStorage.getItem("hour2"));
+  $("#hour3 .taskDescription").val(localStorage.getItem("hour3"));
+  $("#hour4 .taskDescription").val(localStorage.getItem("hour4"));
+
 
   // check to see if each hour is before, after, or equal to the current hour and style accordingly
 
